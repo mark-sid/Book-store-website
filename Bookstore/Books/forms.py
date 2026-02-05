6d1708edd49d38719daf24a7b26c589b3cee282b
Mark <mark.sid2011@gmail.com>
2026-02-05 18:49:39 +0300

    Update forms.py

--- Bookstore/Books/forms.py
+++ Bookstore/Books/forms.py
@@ -9,10 +9,7 @@ class CategorySelectForm(forms.Form):
         to_field_name='slug',
         empty_label='All Categories',
         widget=forms.Select(attrs={'onchange': 'this.form.submit()'}),
-<<<<<<< HEAD
-=======
         required=False
->>>>>>> 54c77019738af15dfe5148add3b6254c0c648837
     )
 
     def __init__(self, *args, **kwargs):
